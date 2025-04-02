09ea84f816358cf839d936da5ca6de86efd5e0d9
Kushal Mishrey <princeprince1052@gmail.com>
2025-04-02 18:00:59 +0545

    cleanup

--- script/class/classContactService.js
+++ script/class/classContactService.js
@@ -1,27 +1,6 @@
 class ContactService {
   static async fetchContacts(courseID) {
     try {
-      // const query = `
-      //   query calcRegisteredMembersRegisteredCoursesMany($registered_course_id: PriestessCourseID) {
-      //     calcRegisteredMembersRegisteredCoursesMany(
-      //       query: [
-      //         {
-      //           where: {
-      //             registered_course_id: $registered_course_id
-      //           }
-      //         }
-      //       ]
-      //     ) {
-      //       Registered_Member_Contact_ID: field(arg: ["Registered_Member", "id"])
-      //       Registered_Member_First_Name: field(arg: ["Registered_Member", "first_name"])
-      //       Registered_Member_Last_Name: field(arg: ["Registered_Member", "last_name"])
-      //       Registered_Member_Profile_Image: field(arg: ["Registered_Member", "profile_image"])
-      //       Registered_Member_Display_Name: field(arg: ["Registered_Member", "display_name"])
-      //       Registered_Member_Forum_Image: field(arg: ["Registered_Member", "forum_image"])
-      //     }
-      //   }
-      // `;
-
       const query = `query calcContacts {
   calcContacts {
     Contact_ID: field(arg: ["id"])
